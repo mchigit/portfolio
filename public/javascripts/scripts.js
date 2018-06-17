3dbd280659ef5b40a482e35f964dd9f50f0a12f4
Michael Chi <michael.chi1997@gmail.com>
2018-06-17 08:28:01 -0400

    Shorten spinner loading

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
         setTimeout(function(){
             $('body').addClass('loaded');
-        }, 1000);
+        }, 500);
 
         $('form').on('submit', function(event) {
             event.preventDefault();
