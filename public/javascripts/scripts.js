4b138e42701e2e15f29b8f6d3762fc1b7dcb1725
Michael Chi <michael.chi1997@gmail.com>
2018-06-17 08:24:01 -0400

    Change loader timeout to 1s

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
         setTimeout(function(){
             $('body').addClass('loaded');
-        }, 2000);
+        }, 1000);
 
         $('form').on('submit', function(event) {
             event.preventDefault();
